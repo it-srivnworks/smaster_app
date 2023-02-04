3f4772cb3e5966fb5b6e8c26952eee7fea1b568d
dev01-srivnworks <dev01.srivnworks@gmail.com>
2023-02-04 13:56:51 +0000

    Aligning flexbox

--- src/components/library/LibraryHome.js
+++ src/components/library/LibraryHome.js
@@ -1,12 +1,79 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 const LibraryHome = () => {
   return (
-    <View>
-      <Text>Library Home!</Text>
+    <View style={styles.compContainer}>
+      <View style={styles.compHeader}>
+        <Text style={styles.compHeaderTxt}>Library</Text>
+      </View>
+      <View style={styles.compBox}>
+        <ScrollView>
+          <View style={styles.infoTab}>
+            <View style={styles.infoBox}>
+              <Text style={styles.infoTxt}>Book List</Text>
+            </View>
+            <View style={styles.infoBox}>
+              <Text style={styles.infoTxt}>Add New</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default LibraryHome
+export default LibraryHome;
+
+const styles = StyleSheet.create({
+  compContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#FFF',
+    justifyContent: 'flex-start',
+  },
+  compHeader: {
+    flexDirection: 'column',
+    backgroundColor: '#083b66',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '10%',
+    borderWidth: 2,
+    borderColor: '#FFF',
+  },
+  compHeaderTxt: {
+    color: '#FFF',
+    fontSize: 24,
+    justifyContent: 'center',
+  },
+  compBox: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#083b66',
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    borderColor: '#FFF',
+  },
+  infoTab: {
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    height: 100,
+    borderColor: '#083b66',
+  },
+  infoBox: {
+    backgroundColor: '#083b66',
+    borderWidth: 5,
+    width: '50%',
+    height: '100%',
+    borderColor: '#FFF',
+    borderRadius: 10,
+  },
+  infoTxt: {
+    color: '#FFF',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
