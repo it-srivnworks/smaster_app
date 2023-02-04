745e7f30e6af9700259ac6d62a28885ed781c66f
dev01-srivnworks <dev01.srivnworks@gmail.com>
2023-02-04 00:42:39 +0000

    Added BootmNav

---
+++ src/components/library/Test1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Test1 = () => {
+  return (
+    <View>
+      <Text>Tester1</Text>
+    </View>
+  )
+}
+
+export default Test1
--- src/components/welcome/HomePage.js
+++ src/components/welcome/HomePage.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import LibraryHome from '../library/LibraryHome'
-import Dashboard from './Dashboard'
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import LibraryHome from '../library/LibraryHome';
+import Test1 from '../library/Test1';
+import Test2 from '../library/Test2';
+import Test3 from '../library/Test3';
+import Dashboard from './Dashboard';
+
+const BottomTab = createMaterialBottomTabNavigator();
 
 const HomePage = () => {
   return (
-    <View>
-      <LibraryHome></LibraryHome>
-      <Dashboard></Dashboard>
-    </View>
-  )
-}
+    <BottomTab.Navigator
+      barStyle={{
+        backgroundColor: '#083b66',
+        borderWidth: 1,
+        borderColor: '#FFF',
+        height:80,
+        borderRadius:80,
+      }}>
+      <BottomTab.Screen
+        name="dashboard"
+        component={Dashboard}></BottomTab.Screen>
+      <BottomTab.Screen
+        name="library"
+        component={LibraryHome}></BottomTab.Screen>
+      <BottomTab.Screen name="test1" component={Test1}></BottomTab.Screen>
+      <BottomTab.Screen name="test2" component={Test2}></BottomTab.Screen>
+    </BottomTab.Navigator>
+  );
+};
 
-export default HomePage
+export default HomePage;
